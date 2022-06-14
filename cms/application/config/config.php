0abb3c68949c6fa9c2b3917b0e666963d4f0a742
drshnkini <alphacoresolutions@gmail.com>
2022-06-14 13:21:24 +0530

    live db connection

--- cms/application/config/config.php
+++ cms/application/config/config.php
@@ -26,7 +26,7 @@ date_default_timezone_set('Asia/Kolkata');
 $root = "https://".$_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
  $config['base_url']    = "$root";
-$config['base_url']    = "http://localhost/solarkitindia.com/cms/";
+$config['base_url']    = "https://solarkitindia.com/cms/";
 /*//////
 |--------------------------------------------------------------------------
 | Index File
